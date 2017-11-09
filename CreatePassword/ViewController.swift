5c8b6925d3913c016f84492a35f75cf6c07eb26c
糊涂 <hutuygah@163.com>
2017-11-09 13:52:14 +0800

    Initial Commit

---
+++ CreatePassword/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CreatePassword
+//
+//  Created by 糊涂 on 2017/11/9.
+//  Copyright © 2017年 糊涂. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
